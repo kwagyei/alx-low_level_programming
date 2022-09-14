2a45f56d3be9389a21eefb7726a2c65e36ece626
root <kwabenasasuboakye@gmail.com>
2022-09-13 19:55:33 -0700

    TAsk 2 3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,13 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints the alphabet 10 times
- *
- * @x: this is the charactor to be checked
- *
- * @count: this is the count
+ * print_alphabet_x10 - this function prints the alphabet 10 times
  *
  * Return: Nothing
  */
 
 void print_alphabet_x10(void)
 {
-/**
- * print_alphabet_x10 - while loop alphapbet is printed 10 times
- *
- */
-
 int x;
 int count = 0;
 
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,18 +1,14 @@
 #include "main.h"
 
 /**
- * _islower - checks for lower case
+ * _islower - this function checks for lower case
  *
- * @c: this is the charactor we are checking
+ * @c: the charactor to be checked
  *
  * Return: 0 or 1
  */
 
 int _islower(int c)
-/**
- * _islower - function to check if character is lowercase and return 1
- */
-
 {
 if (c >= 'a' && c <= 'z')
 {
