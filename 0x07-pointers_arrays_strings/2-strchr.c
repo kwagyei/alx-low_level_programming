4baac23c63fb2060bda22788e9c219d746ffdbe6
root <kwabenasasuboakye@gmail.com>
2022-09-26 15:15:52 -0700

    Task 1 2 3 4 5

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  * _strchr - locate character in string
+  * @s: source string
+  * @c: character to find
+  *
+  * Return: the string from character found
+  */
+char *_strchr(char *s, char c)
+{
+	int x = 0, y;
+
+	while (s[x])
+	{
+		x++;
+	}
+
+	for (y = 0; y <= x; y++)
+	{
+		if (c == s[y])
+		{
+			s += y;
+			return (s);
+		}
+	}
+
+	return ('\0');
+}
